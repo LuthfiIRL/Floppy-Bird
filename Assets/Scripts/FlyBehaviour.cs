8c6a7091f51c9f5ebd26256ff921d6c5b82f27d8
LuthfiIRL <luthfirlangga22@gmail.com>
2025-04-04 20:08:10 +0700

    Adding jump, and pipe
    Adding jump function to bird, and the bird rotate when jumping, adding pipe to the scene, adding MovePipe.cs

--- Assets/Scripts/FlyBehaviour.cs
+++ Assets/Scripts/FlyBehaviour.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class FlyBehaviour : MonoBehaviour
 {
-    [SerializeField] private float velocity = 1.5f;
+    [SerializeField] private float jumpVelocity = 1.5f;
+    [SerializeField] private float rotationSpeed = 10f;
     private Rigidbody2D rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            rb.velocity = Vector2.up * jumpVelocity;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0,0,rb.velocity.y * rotationSpeed);
     }
 }
